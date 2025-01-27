0f22e03875f1fd84c9ebcefe8d5f3857eefef97a
NRG55 <maloj_1980@yahoo.fr>
2025-01-27 01:07:27 +0000

    Add(Index.js): prettyPrint function - console.log a tree in a structured format

--- index.js
+++ index.js
@@ -3,4 +3,18 @@ import Tree from "./tree,js";
 
 const tree = new Tree([8,1,2,19,54,76,10,12,9,77,3,12,4]);
 
-console.log(tree)
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+  prettyPrint(tree.root)
